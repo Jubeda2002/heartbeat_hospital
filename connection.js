81ccb4ba24b1dc77584d367ad59119125fd5e1e3
Jubeda2002 <jshaikh00365@gmail.com>
2025-04-17 18:32:51 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql=require("mysql");
 var util=require("util");
 var conn=mysql.createConnection({
-	"host":"localhost",
-	"user":"root",
-	"password":"",
-	"database":"heartbeat_hospital"
+	"host":"bjggynpor9l8tkxsngoc-mysql.services.clever-cloud.com",
+	"user":"usd6jvwiumne7mgr",
+	"password":"1uk5LmSfuuXvuEqqx22f",
+	"database":"bjggynpor9l8tkxsngoc"
 });
 var exe=util.promisify(conn.query).bind(conn);
 module.exports=exe;
