2b9254b34c28408958787636870e89a1e2a40a97
Jubeda2002 <jshaikh00365@gmail.com>
2025-04-17 19:29:02 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,9 +2,9 @@ var mysql=require("mysql2");
 var util=require("util");
 var conn=mysql.createConnection({
 	"host":"bjnmpeouy2bpkvj9fgrb-mysql.services.clever-cloud.com",
-	"user":"bjnmpeouy2bpkvj9fgrb",
-	"password":"u3pydhi1dzligm6q",
-	"database":"D61WNAGRZVkklIjoGGpj"
+	"user":"u3pydhi1dzligm6q",
+	"password":"D61WNAGRZVkklIjoGGpj",
+	"database":"bjnmpeouy2bpkvj9fgrb"
 });
 var exe=util.promisify(conn.query).bind(conn);
 module.exports=exe;
